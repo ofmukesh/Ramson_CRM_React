ff46065f3b8053ed003b0221b0d080629e10acee
x1mukesh <of.mukesh@gmail.com>
2021-10-24 01:20:48 +0530

    nav bar 20%

---
+++ src/components/Nav/navbar.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function NavBar() {
+  return (
+      <Router>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+              <img src="logo.png" alt="" height="40" width="40" />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarScroll"
+              aria-controls="navbarScroll"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarScroll">
+              <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/home">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/form">
+                   Form 
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
+                    Table
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarScrollingDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Link
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarScrollingDropdown"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <form className="d-flex">
+                <input
+                  className="form-control me-2"
+                  type="text"
+                  placeholder="Feedback"
+                  aria-label="Feedback"
+                />
+                <button className="btn btn-outline-success" type="button">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </Router>
+  );
+}
+
+export default NavBar;

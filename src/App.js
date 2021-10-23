16da60e235cc4f4a22d6016334dac461186ddd2a
x1mukesh <of.mukesh@gmail.com>
2021-10-24 01:30:31 +0530

    new components imported

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import NavBar from "./components/navbar"
+import Home from "./pages/home";
+import LeadForm from "./components/Form/lead_form";
 
+import NavBar from "./components/Nav/navbar";
 
 function App() {
   return (
     <div className="main">
-      <NavBar />
+      <Router>
+        <NavBar />
+      </Router>
+      <Router>
+        <Switch>
+          <Route path="/home" exact component={Home} />
+          <Route path="/form" component={LeadForm} />
+          {/* <Route path="/" component={} /> */}
+          {/* <Route path="/" component={} /> */}
+        </Switch>
+      </Router>
     </div>
   );
 }

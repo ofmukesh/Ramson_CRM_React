909dbef09f1afd1f769a147ecb2af15b080ca7f0
x1mukesh <of.mukesh@gmail.com>
2021-10-24 01:30:52 +0530

    home init

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="main">
+      <div className="container">Home</div>
+    </div>
+  );
+};
+
+export default Home

617df0549d37ff1e9c81f45a38e6bd261e017ac4
x1mukesh <of.mukesh@gmail.com>
2021-10-24 01:30:09 +0530

    simple form

---
+++ src/components/Form/lead_form.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LeadForm = () => {
+  return (
+    <div className="container mt-2 text-center">
+        <div className="row">
+            <form action="" method="post">
+                <input type="text" id="" className="form-control mt-1" placeholder="name"/>
+                <input type="text" id="" className="form-control my-1" placeholder="num"/>
+            </form>
+        </div>
+    </div>
+  );
+};
+
+export default LeadForm;

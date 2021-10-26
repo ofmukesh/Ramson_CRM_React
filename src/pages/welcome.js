0334f38a8f9c06e0f9f02f0143fc2e0eb55112dd
x1mukesh <of.mukesh@gmail.com>
2021-10-26 23:34:30 +0530

    new welcome page

---
+++ src/pages/welcome.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+const Welcome = () => {
+  return (
+    <>
+      <div className="container text-center">
+        <h1>Welcome!!</h1>
+      </div>
+    </>
+  );
+};
+
+export default Welcome;

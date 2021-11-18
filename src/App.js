b3b05702882e34b23cd29d94b88497d3ea6d2c93
x1mukesh <of.mukesh@gmail.com>
2021-11-18 18:13:25 +0530

    new 3 forms

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
-import LeadForm from "./components/Form/lead_form";
+import CSalesForm from "./components/Form/sales_form";
 import Welcome from "./pages/welcome";
 import Contact from "./pages/contact";
 import Profile from "./pages/profile";
@@ -20,7 +20,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Welcome} />
           <Route path="/home" component={Home} />
-          <Route path="/form" component={LeadForm} />
+          <Route path="/form" component={CSalesForm} />
           <Route path="/table" component={Table} />
           <Route path="/contact" component={Contact} />
           <Route path="/profile" component={Profile} />
